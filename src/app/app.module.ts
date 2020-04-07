55238e5ea3eb1086789d121be2be01204bb2780a
Argañaras Tomas <sarganaras@vlocity.com>
2020-04-07 17:33:54 -0300

    more changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,8 @@ import { AdivinaMasListadoComponent } from './componentes/adivina-mas-listado/ad
 import { AgilidadMasListadoComponent } from './componentes/agilidad-mas-listado/agilidad-mas-listado.component';
 import { RuteandoModule } from './ruteando/ruteando.module';
 import { ListadoComponent } from './componentes/listado/listado.component';
-import { JuegoMemoria } from './clases/juego-Memoria';
+
+import { JuegoMemoria } from '../app/clases/juego-memoria';
 import { JuegoPiedraPapelTijera } from './clases/juego-piedra-papel-tijera';
 
 // declaro donde quiero que se dirija
